628768f0aa539ffe8f85c22cfbccd27af5d341ef
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-06-04 23:45:51 +0300

    Pokemon class

---
+++ Java Advanced/Classes/PokemonTrainer/Pokemon.java
@@ -0,0 +1,31 @@
+package Classes.PokemonTrainer;
+
+import java.util.List;
+
+public class Pokemon {
+    private String name;
+    private String element;
+    private int health;
+
+    public Pokemon(String name, String element, int health) {
+        this.name = name;
+        this.element = element;
+        this.health = health;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getElement() {
+        return this.element;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setHealth() {
+         this.health -= 10;
+    }
+}
