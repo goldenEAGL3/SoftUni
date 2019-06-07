3e5bc21775e021edadb2a273f5fa48f0dc92bd6a
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-06-07 22:29:18 +0300

    Item class

---
+++ Java Advanced/Exams/February24th2019/heroRepository/Item.java
@@ -0,0 +1,37 @@
+package Exams.February24th2019.heroRepository;
+
+public class Item {
+    private int strength;
+    private int agility;
+    private int intelligence;
+
+    public Item(int strength, int agility, int intelligence) {
+        this.strength = strength;
+        this.agility = agility;
+        this.intelligence = intelligence;
+    }
+
+    public int getStrength() {
+        return this.strength;
+    }
+
+    public int getAgility() {
+        return this.agility;
+    }
+
+    public int getIntelligence() {
+        return this.intelligence;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Item:%n" +
+                "* Strength: %d%n" +
+                "* Agility: %d%n" +
+                "* Intelligence: %d",
+                this.strength,
+                this.agility,
+                this.intelligence);
+    }
+
+}
