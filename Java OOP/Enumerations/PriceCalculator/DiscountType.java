30b3e18bffcb4df73ddd7dc0d818bdd0c0c3a7ce
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-06-27 20:27:19 +0300

    DiscounType class

---
+++ Java OOP/Enumerations/PriceCalculator/DiscountType.java
@@ -0,0 +1,17 @@
+package Abstraction.PriceCalculator;
+
+public enum DiscountType {
+    VIP(20),
+    SecondVisit(10),
+    None(0);
+
+    int discount;
+
+    DiscountType(int discount) {
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return this.discount / 100.0;
+    }
+}
