08162e2742c2788b1f25b066b52c1b90c348baa2
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-06-28 21:49:04 +0300

    StudentRepository class

---
+++ Java OOP/Enumerations/StudentSystem/StudentRepository.java
@@ -0,0 +1,26 @@
+package Abstraction.StudentSystem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRepository {
+    private Map<String, Student> storage;
+
+    public StudentRepository() {
+        this.storage = new HashMap<>();
+    }
+
+    public boolean exists(String name) {
+        return this.storage.containsKey(name);
+    }
+
+    public Student getByName(String name) {
+        return this.storage.get(name);
+    }
+
+    public void save(Student student) {
+        this.storage.put(student.getName(), student);
+    }
+
+
+}
