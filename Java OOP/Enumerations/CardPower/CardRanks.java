dc912b8289a954c670ed61da9b19e688ce83967c
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-06-29 23:42:00 +0300

    CardRanks enumeration

---
+++ Java OOP/Enumerations/CardPower/CardRanks.java
@@ -0,0 +1,28 @@
+package Abstraction.CardPower;
+
+public enum CardRanks {
+    ACE(14),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13),
+    ;
+
+    private int rank;
+
+    CardRanks(int rank) {
+        this.rank = rank;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+}
