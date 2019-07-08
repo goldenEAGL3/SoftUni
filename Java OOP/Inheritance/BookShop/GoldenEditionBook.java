4a1b23b6fae725bba0e6d5d8eef69d931b75b4ab
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-07-08 23:55:32 +0300

    GoldenEditionBook class

---
+++ Java OOP/Inheritance/BookShop/GoldenEditionBook.java
@@ -0,0 +1,14 @@
+package Inheritance.BookShop;
+
+public class GoldenEditionBook extends Book {
+
+
+    public GoldenEditionBook(String title, String author, double price) {
+        super(title, author, price);
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice() * 1.3;
+    }
+}
