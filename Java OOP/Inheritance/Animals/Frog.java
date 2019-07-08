6dee0bbf24a37e846e8372d25d09a022e8bd58d9
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-07-08 23:55:00 +0300

    Frog class

---
+++ Java OOP/Inheritance/Animals/Frog.java
@@ -0,0 +1,14 @@
+package Inheritance.animals;
+
+public class Frog extends Animal {
+    private final static String SOUND = "Ribbit";
+
+    public Frog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return Frog.SOUND;
+    }
+}
