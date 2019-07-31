610a3682b5a153234e61c37b9dfe5e31cef391b5
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-07-31 23:03:09 +0300

    Motorcycle class

---
+++ Java OOP/Inheritance/NeedForSpeed/Motorcycle.java
@@ -0,0 +1,12 @@
+package Inheritance.NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+
+    }
+
+
+}
