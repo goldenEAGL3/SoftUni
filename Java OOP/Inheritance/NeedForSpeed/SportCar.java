af81333c83cdddb6cb935efeb08301922802a29c
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-07-31 23:03:24 +0300

    SportCar class

---
+++ Java OOP/Inheritance/NeedForSpeed/SportCar.java
@@ -0,0 +1,10 @@
+package Inheritance.NeedForSpeed;
+
+public class SportCar extends Car {
+    public SportCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(10);
+    }
+
+
+}
