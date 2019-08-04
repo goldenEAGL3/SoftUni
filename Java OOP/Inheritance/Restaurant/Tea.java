140e9b8509408f3c191fd8d87a5c82ed8834b4b7
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-04 13:57:42 +0300

    Tea class

---
+++ Java OOP/Inheritance/Restaurant/Tea.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+
+    public Tea(String name, BigDecimal price, double millimeters) {
+        super(name, price, millimeters);
+    }
+}
