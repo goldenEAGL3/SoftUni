1281d81557b5aa50a7f1a4586f4f5fd8b66e5a99
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-04 13:57:01 +0300

    HotBeverage class

---
+++ Java OOP/Inheritance/Restaurant/HotBeverage.java
@@ -0,0 +1,10 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+
+    public HotBeverage(String name, BigDecimal price, double millimeters) {
+        super(name, price, millimeters);
+    }
+}
