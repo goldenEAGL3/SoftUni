0d75fb78e020cd1ab1e478d884099a0e7dd3f05d
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-07 23:34:14 +0300

    MissionImpl class

---
+++ Java OOP/Interfaces and Abstraction/MilitaryElite/Common/MissionImpl.java
@@ -0,0 +1,19 @@
+package InterfacesAndAbstractions.MilitaryElite.Common;
+
+import InterfacesAndAbstractions.MilitaryElite.enumerations.State;
+import InterfacesAndAbstractions.MilitaryElite.interfaces.Mission;
+
+public class MissionImpl implements Mission {
+    private String codeName;
+    private State state;
+
+    public MissionImpl(String codeName, State state) {
+        this.codeName = codeName;
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Code Name: %s State: %s", this.codeName, this.state.toString());
+    }
+}
