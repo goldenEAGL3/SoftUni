ee73ba14382685a2171db53a980098a22c168a0c
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-10 11:50:35 +0300

    CutTransform class

---
+++ Java OOP/Polymorphism/Words/CutTransform.java
@@ -0,0 +1,16 @@
+package Polymorphism.Words;
+
+public class CutTransform implements TextTransform {
+    private StringBuilder lastCut;
+
+    @Override
+    public void invokeOn(StringBuilder text, int startIndex, int endIndex) {
+        this.lastCut = new StringBuilder();
+        this.lastCut.append(text, startIndex, endIndex);
+        text.delete(startIndex, endIndex);
+    }
+
+    public StringBuilder getLastCut() {
+        return this.lastCut;
+    }
+}
