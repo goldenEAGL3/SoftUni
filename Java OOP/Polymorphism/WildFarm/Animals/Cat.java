8b9531d2de5590e80abed2f19d2aa4ee3cc56b8a
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-11 23:38:10 +0300

    Cat class

---
+++ Java OOP/Polymorphism/WildFarm/Animals/Cat.java
@@ -0,0 +1,41 @@
+package Polymorphism.WildFarm.Animals;
+
+import Polymorphism.WildFarm.Food.Food;
+
+import java.text.DecimalFormat;
+
+public class Cat extends Felime {
+    private String breed;
+
+    public Cat(String animalType, String animalName, Double animalWeight, String livingRegion, String breed) {
+        super(animalType, animalName, animalWeight, livingRegion);
+        this.breed = breed;
+    }
+
+
+    private String getBreed() {
+        return this.breed;
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meowwww");
+    }
+
+    @Override
+    public void eat(Food food) {
+        super.setFoodEaten(super.getFoodEaten() + food.getQuantity());
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return String.format("%s[%s, %s, %s, %s, %d]",
+                super.getAnimalType(),
+                super.getAnimalName(),
+                this.getBreed(),
+                df.format(super.getAnimalWeight()),
+                super.getLivingRegion(),
+                super.getFoodEaten());
+    }
+}
