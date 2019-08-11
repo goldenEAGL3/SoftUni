1753af619fab88417d8e69af67baa00f4f64265a
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-11 23:38:15 +0300

    Felime class

---
+++ Java OOP/Polymorphism/WildFarm/Animals/Felime.java
@@ -0,0 +1,7 @@
+package Polymorphism.WildFarm.Animals;
+
+public abstract class Felime extends Mammal {
+    public Felime(String animalType, String animalName, Double animalWeight, String livingRegion) {
+        super(animalType, animalName, animalWeight, livingRegion);
+    }
+}
