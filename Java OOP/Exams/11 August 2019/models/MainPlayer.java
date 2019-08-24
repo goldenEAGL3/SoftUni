239155609f9cc71c80e3e85e62d73ed8fb16a1cc
Todor Filev <48495509+goldenEAGL3@users.noreply.github.com>
2019-08-24 22:37:27 +0300

    MainPlayer class

---
+++ Java OOP/Exams/11 August 2019/models/MainPlayer.java
@@ -0,0 +1,13 @@
+package viceCity.models;
+
+
+public class MainPlayer extends BasePlayer {
+    private static final String DEFAULT_NAME = "Tommy Vercetti";
+        private static final int DEFAULT_LIFE_POINTS = 100;
+
+    public MainPlayer() {
+        super(DEFAULT_NAME, DEFAULT_LIFE_POINTS);
+    }
+
+
+}
